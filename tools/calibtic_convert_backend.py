feb51c2c6c99227c7fbfe132f56bf118d53694af
Jose Luis Montes Marrero <jose.montes@kip.uni-heidelberg.de>
2020-04-18 15:24:38 +0200

    Reuse calibtic_convert_backend to evaluate backend integrity
    Change-Id: I8d60c1d2ea7b5e26e0779dcfed6db0b613c926f7

--- tools/calibtic_convert_backend.py
+++ tools/calibtic_convert_backend.py
@@ -41,24 +41,26 @@ def load_backend(path, ext):
 def main(inpath, inext, outpath, outext, wafer, hicann):
     """
     converts inname to outname based on filename extensions
+    If no outpath is received, only load the input backend,
+    which can be used as a file integrity check.
     """
 
     in_backend = load_backend(inpath, inext)
-    out_backend = load_backend(outpath, outext)
+    if outpath: out_backend = load_backend(outpath, outext)
 
     in_hc = pycalibtic.HICANNCollection()
     in_md = pycalibtic.MetaData()
 
     in_backend.load(get_calibtic_name(wafer, hicann), in_md, in_hc)
-    out_backend.store(get_calibtic_name(wafer, hicann), in_md, in_hc)
+    if outpath: out_backend.store(get_calibtic_name(wafer, hicann), in_md, in_hc)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('inpath')
-    parser.add_argument('inext')
-    parser.add_argument('outpath')
-    parser.add_argument('outext')
+    parser.add_argument('--inpath', type=str, required=True, help='Path of input calibration file')
+    parser.add_argument('--inext', type=str, required=True, choices=['xml', 'dat', 'txt'], help='Extension of input calibration file')
+    parser.add_argument('--outpath', type=str, required=False, help='Path of output calibration file')
+    parser.add_argument('--outext', type=str, required=False, choices=['xml', 'dat', 'txt'], help='Extension of output calibration file')
     add_default_coordinate_options(parser)
 
     args = parser.parse_args()
